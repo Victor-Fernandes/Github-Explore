50cf34705dc0d9e114809b3a88a70987ce486407
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-05-05 14:34:17 -0300

    add tratamento de erro, salvar no localstorage, consumir api"

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 import logo from '../../assets/logogithub.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 // esse formato permite a tipagem de um componente de forma mais facil
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    // salvar no local storage
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault(); // nao recarregar page
+    if (!newRepo) {
+      setInputError('Digiter autor/nome do repositório');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca do repositório.');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="GitHub Explorer" />
       <Title>Explore Repositórios no GitHub</Title>
-      <Form>
-        <input type="text" placeholder="Digite o nome do repsitório." />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repsitório."
+        />
         <button type="submit">Pesquisar</button>
       </Form>
+      {inputError && <Error>{inputError}</Error>}
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/34258399?s=460&u=4c62694aa04a219a7a3a30d00e625809cddb06f5&v=4"
-            alt="profile"
-          />
-          <div>
-            <strong>Victor-Fernandes/Github-Explorer</strong>
-            <p>Search and find repositories in Github.</p>
-          </div>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/34258399?s=460&u=4c62694aa04a219a7a3a30d00e625809cddb06f5&v=4"
-            alt="profile"
-          />
-          <div>
-            <strong>Victor-Fernandes/Github-Explorer</strong>
-            <p>Search and find repositories in Github.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/34258399?s=460&u=4c62694aa04a219a7a3a30d00e625809cddb06f5&v=4"
-            alt="profile"
-          />
-          <div>
-            <strong>Victor-Fernandes/Github-Explorer</strong>
-            <p>Search and find repositories in Github.</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
 
+interface RepositoryParams {
+  repository: string;
+}
 // esse formato permite a tipagem de um componente de forma mais facil
 const Repository: React.FC = () => {
+  const { params } = useRouteMatch();
+
   return <h1>Repository</h1>;
 };
 
