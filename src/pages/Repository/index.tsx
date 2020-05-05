ffd09cedfa6538f803dd6dbc7b4649bdc21b2ae6
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-05-05 16:59:37 -0300

    finalizando estilo da page repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logogithub.svg';
 
 interface RepositoryParams {
   repository: string;
 }
 // esse formato permite a tipagem de um componente de forma mais facil
 const Repository: React.FC = () => {
-  const { params } = useRouteMatch();
+  const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository</h1>;
+  useEffect(() => {
+    api.get(`/repos/${params.repository}`).then((response) => {});
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explore" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          back
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="rocket"
+          />
+          <div>
+            <strong>rokect/unform</strong>
+            <p>descrição do repo</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to="kkk">
+          <div>
+            <strong>qalqercoisa</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
